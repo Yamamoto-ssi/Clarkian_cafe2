e21f5fa80fd1fb414f397df56a1bc111c150b60e
Yamamoto-ssi <brixsedillo@gmail.com>
2026-04-26 00:41:24 +0800

    Add files via upload

--- Cafe_ccst/obj/Release/net8.0-windows7.0/Cafe_ccst.AssemblyInfo.cs
+++ Cafe_ccst/obj/Release/net8.0-windows7.0/Cafe_ccst.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cafe_ccst")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0686d22458f71e6a6554e9eebb8327a9bfd3e27a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cafe_ccst")]
